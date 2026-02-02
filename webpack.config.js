ed5d3867eb09a21a061365cfdda0eabee3325057
Giuliana Villarroel <giu.villarroel@gmail.com>
2026-02-02 13:52:34 -0300

    Initial commit: configuración de Webpack y estructura de archivos

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	devServer: {
+		static: "./dist",
+		hot: true,
+		open: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			title: "Todo List App",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+};
