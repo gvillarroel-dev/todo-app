0d7cdd07b8a68ec9c56abd3c75bf73fc004fbe23
Giuliana Villarroel <giu.villarroel@gmail.com>
2026-03-05 11:47:45 -0300

    feat(tasks): add tasksEvents.js to manage project group interactions - Handles the initialization and toggling of project group visibility.

---
+++ src/events/tasksEvents.js
@@ -0,0 +1,12 @@
+export const initTasksEvents = () => {
+	const headers = document.querySelectorAll(".project-group__header");
+	headers.forEach((header) => {
+		header.addEventListener("click", () => {
+			const table = header.nextElementSibling;
+			table.toggleAttribute("hidden");
+
+			const span = header.querySelector("span");
+			span.classList.toggle("collapsed");
+		});
+	});
+};
